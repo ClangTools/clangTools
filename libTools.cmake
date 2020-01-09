f12eda3b644763077377020e5cbd09ca318c961d
Caesar <Caesar@kekxv.com>
2020-01-09 16:39:14 +0800

    fix cmake bug

--- libTools.cmake
+++ libTools.cmake
@@ -1,3 +1,9 @@
 
 set(libTools_LIBRARIES Tools)
-FILE(GLOB libTools_INCLUDE_DIR "src/*")
+
+find_path(libTools_DIR "libTools.cmake" DOC "Root directory of libTools")
+if (EXISTS "${libTools_DIR}/libTools.cmake")
+    FILE(GLOB libTools_INCLUDE_DIR "${libTools_DIR}/src/*")
+else ()
+    message(FATAL_ERROR "cannot find libTools")
+endif ()
