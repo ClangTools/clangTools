79c37bb1ab53f2a3bcc45ca329a8cb037c0876c8
Caesar <caesar@kekxv.com>
2020-03-12 14:42:11 +0800

    fix submodel install.cmake

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,24 +1,26 @@
 
-########安装脚本##########
-install(TARGETS ${libTools_LIBRARIES}
-        RUNTIME DESTINATION lib
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib)
+if ("${Tools_Other_Project}" STREQUAL "ON")
+    ########安装脚本##########
+    install(TARGETS ${libTools_LIBRARIES}
+            RUNTIME DESTINATION lib
+            LIBRARY DESTINATION lib
+            ARCHIVE DESTINATION lib)
 
-FILE(GLOB srcInstallSourceHeader "src/*")
+    FILE(GLOB srcInstallSourceHeader "src/*")
 
-set(include_header "")
-foreach (item in ${srcInstallSourceHeader})
-    FILE(GLOB subSrcSource "${item}/*.h*")
-    list(APPEND include_header ${subSrcSource})
-endforeach ()
+    set(include_header "")
+    foreach (item in ${srcInstallSourceHeader})
+        FILE(GLOB subSrcSource "${item}/*.h*")
+        list(APPEND include_header ${subSrcSource})
+    endforeach ()
 
-install(FILES
-        ${include_header}
-        DESTINATION include)
+    install(FILES
+            ${include_header}
+            DESTINATION include)
 
-# 复制 lib 代码到 lib 下
-install(DIRECTORY Example/ DESTINATION Example
-        PATTERN "Example/*"
-        PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
-        GROUP_EXECUTE GROUP_READ)
+    # 复制 lib 代码到 lib 下
+    install(DIRECTORY Example/ DESTINATION Example
+            PATTERN "Example/*"
+            PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
+            GROUP_EXECUTE GROUP_READ)
+endif ()
